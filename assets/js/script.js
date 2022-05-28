5bec723a18631b08eb3b4ef5373151b5b2c33b37
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-05-28 14:30:12 -0700

    added saves to the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,7 @@ var loadTasks = function() {
       inProgress: [],
       inReview: [],
       done: []
+      
     };
   }
 
@@ -46,9 +47,37 @@ var saveTasks = function() {
 };
 
 $(".list-group").on("click", "p", function(){
-  console.log("<p> was clicked");
+  var text = $(this)
+  .text() 
+  .trim();
 });
 
+var textInput = $("<textarea>")
+.addClass("form-control")
+.val(text);
+
+$*(this).replaceWith(textInput);
+
+textInput.trigger("focus");
+
+$(".list-group").on("blur", "textarea", function() {
+//get the textarea's current value/text
+var text = $*(this)
+.val()
+.trim();
+
+
+//get parent ul's id attribute
+var status = $(this)
+.closest(".list-group")
+.attr("id")
+.replace("list-", "");
+
+//get task's position in the list of other li elements
+var index = $(this)
+.closest(".list-group-item")
+.index();
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
